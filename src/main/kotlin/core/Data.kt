f462f1d9a07a0e6dc7401f7083f5a552bf1c2eed
Hampus <hampus.hauffman@wearelevels.com>
2022-02-24 19:31:00 +0100

    added projects

--- src/main/kotlin/core/Data.kt
+++ src/main/kotlin/core/Data.kt
@@ -3,4 +3,13 @@ package core
 data class Data(
     val yes: String,
     val colors: List<String>,
+    val projects: List<Project>
+)
+
+data class Project(
+    val days: Int,
+    val lastDate: Int,
+    val amountOfRoles: Int,
+    val score: Int,
+
 )
